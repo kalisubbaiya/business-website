ba209bb7857dd4605bd95c660856471a7f306e03
kaliraj <kalisdvk96@gmail.com>
2022-11-28 19:26:04 +0530

    add my business-website

---
+++ main.js
@@ -0,0 +1,36 @@
+$('.tmoni-carousel').owlCarousel({
+	center:true,
+	items:1,
+    loop:true,
+    margin:10,
+	autoplay:true,
+    responsive:{
+		0:{
+			items:1
+		},
+        600:{
+            items:2
+        },
+		1000:{
+			items:3 
+		}
+    }
+})
+$('.gallery-carousel').owlCarousel({
+	center: true,
+	items:1,
+    loop:true,
+    margin:20,
+	autoplay: true,
+    responsive:{
+		0:{
+			items:1
+		},
+        600:{
+            items:3
+        },
+		1000:{
+			items:5
+		}
+    }
+});
