9ea25bd1e0b2a347b87e61a8ec2e7bbc44ef7af1
kaliraj <kalisdvk96@gmail.com>
2023-01-04 11:34:05 +0530

    medicio website desing using bootstrap

--- main.js
+++ main.js
@@ -15,3 +15,21 @@ $('.tmoni-carousel').owlCarousel({
 		}
     }
 });
+$('.gallery-carousel').owlCarousel({
+	center:true,
+	autoplay:true,
+	items:1,
+	loop:true,
+	margin:20,
+	responsive:{
+		0:{
+			items:1
+		},
+		600:{
+			items:3
+		},
+		1000:{
+			items:5
+		}
+	}
+});
